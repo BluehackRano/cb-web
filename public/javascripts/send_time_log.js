1f8c8ba8ef587e863bb4a5c030787882b56aa959
kyewon <k@bluehack.net>
2018-03-14 19:20:16 +0900

    로그형식 변경 디포폴 생성, 수정, 플랫폼연동 수정

--- public/javascripts/send_time_log.js
+++ public/javascripts/send_time_log.js
@@ -74,3 +74,31 @@ var sendAPILog = function (log_id, api_code, api_provider_code, start_time, end_
     }
   });
 }
+
+var sendTaskAPILog = function (log_id, task_code, api_code, api_provider_code, start_time, end_time, remark) {
+
+  var data = {
+    log_id: log_id,
+    application: 'chatbrick_web',
+    task_code: task_code,
+    api_code: api_code,
+    api_provider_code: api_provider_code,
+    origin: "client",
+    start: start_time,
+    end: end_time,
+    remark: remark
+  };
+
+  $.ajax({
+    url: 'https://www.chatbrick.io/api/log/',
+    type: 'PUT',
+    // async: false,
+    crossDomain: true,
+    xhrFields: {withCredentials: true},
+    contentType: 'application/json',
+    data: JSON.stringify(data),
+    success: function (body, textStatus, res) {
+      console.log('success body=>' + JSON.stringify(body));
+    }
+  });
+}

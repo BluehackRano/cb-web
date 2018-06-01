6e2e828ee35bf91005ff6941dc887a026e30b227
kyewon <k@bluehack.net>
2018-06-01 14:56:49 +0900

    변수선언 누락 버그 픽스

--- public/javascripts/send_time_log.js
+++ public/javascripts/send_time_log.js
@@ -1,27 +1,3 @@
-var sendTimeLog = function (setId, start_time, end_time, tag, remark) {
-  $.ajax({
-    url: 'https://www.chatbrick.io/api/log/',
-    type: 'POST',
-    // async: false,
-    crossDomain: true,
-    xhrFields: {withCredentials: true},
-    data: {
-      setId : setId,
-      platform: 'web',
-      start: start_time,
-      end: end_time,
-      tag: tag,
-      // tag: '페북 페이지 등록',
-      remark: remark
-      // remark: '페북 페이지 등록을 위해 사용'
-
-    },
-    success: function (body, textStatus, res) {
-      console.log('success body=>' + JSON.stringify(body));
-    }
-  });
-}
-
 //changed task log
 var sendTaskLog = function (log_id, task_code, start_time, end_time, remark) {
 
